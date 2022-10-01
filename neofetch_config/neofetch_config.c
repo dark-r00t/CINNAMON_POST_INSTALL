56274c4c33beade1fa9cfae122ed68494f6a1852
Jan D <79925808+dark-r00t@users.noreply.github.com>
2022-09-30 23:04:41 -0400

    Update neofetch_config.c

--- neofetch_config/neofetch_config.c
+++ neofetch_config/neofetch_config.c
@@ -18,15 +18,38 @@ int main(void) {
 
 	if (x == 1) {
 		printf("\nStandard neofetch config loaded!\n\n");
+	
 	} else if (x == 2) {
 		printf("\nFull neofetch config loaded!\n\n");
 		system("cp ~/.config/neofetch/config_full.conf ~/.config/neofetch/config.conf");
+	
 	} else if (x == 3) {
 		printf("\nAlternative neofetch config loaded!\n\n");
 		system("cp ~/.config/neofetch/config_alt.conf ~/.config/neofetch/config.conf");
+
+		char* path = (char*) malloc(sizeof(char) * 200);
+		getcwd(path, 200);
+
+		int i,p=0;
+		for(i=0; p < 3; i++) {
+			if(path[i]=='/') p++;
+		} path[i]='\0';
+
+		// echo "image_source=\"/home/anon/.config/neofetch/ascii_art.txt\"" >> ~/.config/neofetch/config.conf
+		char* path_ = (char*) malloc(sizeof(char) * (99 + i) + 1);
+		strcat(path_, "echo \"image_source=\\\"");
+		strcat(path_, path);
+		strcat(path_, ".config/neofetch/ascii_art.txt\\\"\" >> ~/.config/neofetch/config.conf");
+
+		system(path_);
+
+		free(path);
+		free(path_);
+
 	} else {
 		printf("\nBad input! Retrying...\n\n");
 		goto TOP;
+	
 	}
 
 	system("neofetch");
