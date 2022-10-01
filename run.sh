7825aeebed41ab293aac789ecd7d136ede571155
Jan D <79925808+dark-r00t@users.noreply.github.com>
2022-10-01 10:54:19 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,14 +5,11 @@ chmod +x *.sh
 ./2-yay_get.sh
 ./3-yay_install.sh
 
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
 # sudo pacman -S amd-ucode OR intel-ucode
 sudo systemctl enable lightdm
 systemctl --user enable pipewire
- 
-sudo systemctl status lightdm
-sudo systemctl status pipewire
-sudo systemctl status tor  
-sudo systemctl status sshd
 
 git clone https://github.com/dark-r00t/SUCCY_VODKA.git; cd SUCCY_VODKA
 
@@ -34,6 +31,8 @@ sudo ./../a-dircheck_delete.sh ~/.config/neofetch
 mkdir ~/.config/neofetch
 sudo mv config/neofetch/* ~/.config/neofetch/
 
+mv config/starship.toml ~/.config/
+
 unzip vim_plugs.zip -d ~/
 
 cd ../
@@ -43,6 +42,11 @@ cd neofetch_config/
 make && ./neofetch_config
 
 mkdir ~/Pictures/ ~/Documents/ ~/Downloads/ ~/Music/ ~/Pictures/ ~/Videos/ 
+
+sudo systemctl status lightdm
+sudo systemctl status pipewire
+sudo systemctl status tor  
+sudo systemctl status sshd
 
 cd ../
 sudo rm -r ../CINNAMON_POST_INSTALL
